b23c7676bd07a03d133565ecd47e25b04a4ee4ca
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-07-12 12:20:13 -0300

    inital project Home

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,39 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { CapelakutStyles } from "../src/lib/CapelaKutCommons";
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    font-size: 16px;
   }
-`
+
+  body {
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${CapelakutStyles}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +43,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
