72cdd6ffdb273e313fdfcbf885f6bf272e367914
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-07-14 07:29:19 -0300

    fix: document Head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,29 +1,30 @@
-import Document, { Head } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const page = renderPage(
+      App => props => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {this.props.styleTags}
+          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
+
+export default MyDocument;
